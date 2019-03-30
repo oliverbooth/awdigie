oliverbooth/awdigie

commit 5cf6e1ccb856be98d621353c70782dd6f1755e62

🐛 Fix out-of-bound index errors
clamp $i to 1 and count($bren) to avoid going beyond index limits

includes.php
@@ -9,6 +9,8 @@ $brset = explode("^^",$new_file[1]);
 $i=0;
 foreach(file("$datapath") as $line){
 $br = explode("^^",$line); 
+$i=min($i, count($bren));
+$i=max($i, 1);
 
 if($bren[($i-1)] == "chan_enabled" or $br[0][0] !== "c"){
 

commit 550f195a885911f0e8f07dbef31554c3bdc9bee0

🐛 Fix bug that commit 5cf6e1c introduced
count($bren)-1 because indexes start at 0.
Don't worry about it

includes.php
@@ -9,7 +9,7 @@ $brset = explode("^^",$new_file[1]);
 $i=0;
 foreach(file("$datapath") as $line){
 $br = explode("^^",$line); 
-$i=min($i, count($bren));
+$i=min($i, count($bren) - 1);
 $i=max($i, 1);
 
 if($bren[($i-1)] == "chan_enabled" or $br[0][0] !== "c"){